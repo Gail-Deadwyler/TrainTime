36ce289ea3fe160f825e480e2f97f7ef0e1befb1
rivendell <webwyler@gmail.com>
2019-07-27 22:46:28 -0500

    initialize firebase

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,3 +6,15 @@
 // 4. Create a way to retrieve trains from the trainlist.
 // 5. Create a way to calculate the time way. Using difference between start and current time.
 // 6. Then take the difference and modulus by frequency. (This step can be completed in either 3 or 4)
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCcPFcbAjIsgXGQwE-A3AcOXkeD40qypE8",
+    authDomain: "train-times-93583.firebaseapp.com",
+    databaseURL: "https://train-times-93583.firebaseio.com",
+    storageBucket: "train-times-93583.appspot.com"
+  };
+  
+  firebase.initializeApp(config);
+  
+  var trainData = firebase.database();
